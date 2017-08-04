cfc06d021a1a32cb64df98e22843e355ff14cecd
ni247 <xbc571@gmail.com>
2017-08-04 21:11:24 +0800

    8月4号晚

---
+++ heima_day37_aop/src/com/itheima/demo1/CustomerImpl.java
@@ -0,0 +1,19 @@
+package com.itheima.demo1;
+
+public class CustomerImpl implements ICustomer {
+
+	@Override
+	public void save() {
+		System.out.println("保存客户");
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update() {
+		System.out.println("修改客户");
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ heima_day37_aop/src/com/itheima/demo1/ICustomer.java
@@ -0,0 +1,7 @@
+package com.itheima.demo1;
+
+public interface ICustomer {
+	public void save();
+
+	public void update();
+}
